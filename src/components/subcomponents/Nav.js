8e4565bfe19dca87ae22e01d3272e231572d1335
EpicBlue1 <21100115@virtualwindow.co.za>
2022-05-05 09:27:50 +0200

    final tweaks for dash.
    navigation not set up yet

--- src/components/subcomponents/Nav.js
+++ src/components/subcomponents/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import NavImg from '../../img/NavImg.png'
 
 const navBar = () => {
     return (
         <div className="NavigationBar">
+            <div style={{ backgroundImage: 'url(' + NavImg + ')', backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}} className="NavImg"></div>
         </div>
     );
 };
--- src/components/subcomponents/timeSlot.js
+++ src/components/subcomponents/timeSlot.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const timeSlot = () => {
     return (
-        <div type='text' className="timeSlot"></div>
+        <div type='text' className="timeSlot"><b>15:30</b></div>
     );
 };
 
